982382265b9609139f99bcebb32a53590732e1e3
Andrius K <andkubilius@gmail.com>
2021-09-10 17:00:21 +0300

    added build

--- src/components/Actor/index.js
+++ src/components/Actor/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 // styles
-import { Wrapper, Image} from './Actor.styles';
+import { Wrapper, Image } from './Actor.styles';
 
-const Actor = ({name, character, imageUrl}) => (
-    <Wrapper>
-        <Image src={imageUrl} alt='actor-thumb' />
-        <h3>{name}</h3>
-        <p>{character}</p>
-    </Wrapper>
+const Actor = ({ name, character, imageUrl }) => ( <
+    Wrapper >
+    <
+    Image src = { imageUrl }
+    alt = 'actor-thumb' / >
+    <
+    h3 > { name } < /h3> <
+    p > { character } < /p> <
+    /Wrapper>
 );
 
 export default Actor;
